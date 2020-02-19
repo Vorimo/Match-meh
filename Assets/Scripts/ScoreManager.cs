50e258bfcc414fa7d4c95542ae082aa3b58ddb49
Vorimo <ilyaivanov654@gmail.com>
2020-02-19 20:22:57 +0300

    Score bar, score goals

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,9 +2,15 @@
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
+    private Board board;
     public Text scoreText;
-
     public int score;
+    public Image scoreBar;
+
+    private void Start() {
+        board = FindObjectOfType<Board>();
+        UpdateBar();
+    }
 
     // Update is called once per frame
     private void Update() {
@@ -13,5 +19,14 @@ public class ScoreManager : MonoBehaviour {
 
     public void IncreaseScore(int amountToIncrease) {
         score += amountToIncrease;
+        UpdateBar();
+    }
+
+    private void UpdateBar() {
+        if (board != null && scoreBar != null) {
+            var length = board.scoreGoals.Length;
+            // cast for correct arithmetic operation
+            scoreBar.fillAmount = (float)score / (float)board.scoreGoals[length - 1];
+        }
     }
 }
