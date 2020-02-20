c0204af48a5a094a2cf741d4bac4a2eee52041e1
Vorimo <ilyaivanov654@gmail.com>
2020-02-20 21:08:06 +0300

    Animated menus

---
+++ Assets/Scripts/FadePanelController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FadePanelController : MonoBehaviour {
+    public Animator panelAnim;
+    public Animator gameInfoAnim;
+
+    public void HandleOkButton() {
+        if (panelAnim != null && gameInfoAnim != null) {
+            panelAnim.SetBool("Out", true);
+            gameInfoAnim.SetBool("Out", true);
+        }
+    }
+}
